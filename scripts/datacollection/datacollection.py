741125a1dda6090d9d42ba054d4a8efd86b6dde6
Yutong Gu <gyutong@gmil.com>
2019-07-31 20:29:39 -0700

    added function for searching for user based on geolocation, term, and time. Updated demo datacollection to use these calls.

--- scripts/datacollection/datacollection.py
+++ scripts/datacollection/datacollection.py
@@ -2,9 +2,10 @@ import twitter
 import random
 from datetime import datetime
 import time
+import os
 import pickle
 
-def get_valid_users(api = None, num_ids = 0, run_limit = None):
+def get_random_users(api, num_ids, run_limit = None):
 	assert isinstance(api, twitter.Api)
 	assert num_ids > 0 and num_ids < 10000
 	ids_left_to_gen = num_ids
@@ -32,7 +33,45 @@ def get_valid_users(api = None, num_ids = 0, run_limit = None):
 
 		print("Searching for {} ids".format(100))
 		retval = api.UsersLookup(user_id = ids_list, include_entities = False)
-		users += retval
+		users += [x.id_str for x in retval]
+		print("Got back {} valid ids".format(len(retval)))
+		print("Total valid ids = {}".format(len(users)))
+
+		ids_left_to_gen = num_ids - len(users)
+		old_list = ids_list
+		ids_list = []
+
+	if len(users) > num_ids:
+		print("Got more than we wanted, removing {} users".format(ids_left_to_gen))
+		del users[ids_left_to_gen:]
+
+	return users
+
+def search_users(api, num_ids, term, geocode=None, until=None, since=None, run_limit = None):
+	assert isinstance(api, twitter.Api)
+	assert num_ids > 0 and num_ids < 10000
+	ids_left_to_gen = num_ids
+	ids_list = []
+	old_list = []
+	tweets = []
+	users = []
+
+	while ids_left_to_gen > 0:
+		print(api.CheckRateLimit("https://api.twitter.com/1.1/search/"))
+		if run_limit is not None:
+			if run_limit == 0:
+				break
+			else:
+				run_limit -= 1
+
+		print("Searching for {} ids".format(100))
+		retval = api.GetSearch(term=term, geocode=geocode, until="2019-07-25", count=100)
+		tweets += retval
+		for tweet in tweets:
+			print("Tweet:"+tweet.created_at)
+			if tweet.id_str not in users:
+				print("Appending user {}".format(tweet.id_str))
+				users.append(tweet.id_str)
 		print("Got back {} valid ids".format(len(retval)))
 		print("Total valid ids = {}".format(len(users)))
 
@@ -49,11 +88,14 @@ def get_valid_users(api = None, num_ids = 0, run_limit = None):
 def save_users(user_list, file_name="user_list"):
 	assert isinstance(user_list, list)
 	assert len(user_list) > 0 
-	assert isinstance(user_list[0], twitter.models.User)
 
 	pickle.dump(user_list, open( "datacollection/storage/{}.db".format(file_name), "wb" ))
 
 def load_users(file_name="user_list"):
-	user_list = pickle.load( open( "datacollection/storage/{}.db".format(file_name), "rb" ))
+	file_name = "datacollection/storage/{}.db".format(file_name)
+	if os.path.exists(file_name):
+		user_list = pickle.load( open( file_name, "rb" ))
+	else:
+		return []
 
 	return user_list
--- scripts/demo_datacollection.py
+++ scripts/demo_datacollection.py
@@ -2,16 +2,20 @@ from api_libs.twitterapi import *
 import datacollection.datacollection as datacollection
 
 def main():
-	users = datacollection.get_valid_users(twitter_api, 10)
-	datacollection.save_users(users)
-	
-	print("Saved the following users:")
-	print(users)
-	
 	loaded_users = datacollection.load_users()
-	
 	print("Loaded the following users:")
 	print(loaded_users)
+
+	random_users = datacollection.get_random_users(twitter_api, 10)
+	print("Generated the following random users:")
+	print(random_users)
+
+	searched_users = datacollection.search_users(twitter_api, 10, term='Trump')
+	print("Got the following users from searching Trump")
+	print(searched_users)
+
+	print("Saving searched users")
+	datacollection.save_users(searched_users)	
 
 if __name__ == '__main__':
 	main()
